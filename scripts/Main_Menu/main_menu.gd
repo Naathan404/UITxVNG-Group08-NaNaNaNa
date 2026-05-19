0fdc279a078a8b4939e74c2e80c70b2995036057
Dang Quang <ledangquangtht@gmail.com>
2026-05-19 15:41:12 +0700

    tạo main-menu

---
+++ scripts/Main_Menu/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+# Called when the node enters the scene tree for the first time.
+var game_play = preload("res://dummy_level.tscn")
+
+func _on_start_pressed() -> void:
+	#$fade_animation/AnimationPlayer.play_backwards("fade_in")
+	#await $fade_animation/AnimationPlayer.animation_finished
+	get_tree().change_scene_to_packed(game_play)
+
+func _on_setting_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
