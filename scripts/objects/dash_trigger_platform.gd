24dcacee7ec9f70d882805848634e26691786b1a
Nguyen Nguyen <nathannguyen6002@gmail.com>
2026-05-22 15:08:21 +0700

    feat: level 4

--- scripts/objects/dash_trigger_platform.gd
+++ scripts/objects/dash_trigger_platform.gd
@@ -4,14 +4,22 @@ extends StaticBody2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var magic_dust: CPUParticles2D = $MagicDust
 @onready var burst_dust: CPUParticles2D = $BurstDust
+
+@export_group("Settings")
+@export var hidden_start_state: bool = true
 @export var burst_duration: float = 0.5
 @export var ghost_alpha: float = 0.3
+
 var is_ghost: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameManager.dash_trigger.connect(_handle_state)
-	_become_ghost()
+	magic_dust.emitting = true
+	if hidden_start_state:
+		_become_ghost()
+	else:
+		_become_solid()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,9 +37,9 @@ func _become_ghost() -> void:
 	if is_ghost: return
 	is_ghost = true
 	collision.set_deferred("disabled", true)
-	modulate.a = ghost_alpha
+	sprite.modulate.a = ghost_alpha
 	burst_dust.emitting = true
-	magic_dust.emitting = true
+	
 
 	var tween = create_tween()
 	tween.tween_property(sprite, "scale", Vector2(0.8, 0.8), burst_duration / 3.0).set_trans(Tween.TRANS_SINE)
@@ -40,9 +48,8 @@ func _become_ghost() -> void:
 func _become_solid() -> void:
 	if not is_ghost: return
 	is_ghost = false
-	modulate.a = 1.0
+	sprite.modulate.a = 1.0
 	collision.set_deferred("disabled", false)
-	magic_dust.emitting = false
 	burst_dust.emitting = true
 	
 	sprite.scale = Vector2(0.5, 0.5)
