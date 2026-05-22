7ce00d8ecbc3c7f93ded8ad2886120aa56ec18b5
Dang Quang <ledangquangtht@gmail.com>
2026-05-22 17:12:00 +0700

    boss start

--- scripts/boss/bullet.gd
+++ scripts/boss/bullet.gd
@@ -6,30 +6,23 @@ extends Area2D
 @onready var sprite: Sprite2D = $Sprite2D
 
 var direction: Vector2 = Vector2.ZERO
-var player: Node2D
+var player: Player
+@export var texture_red: Texture2D
+@export var texture_blue: Texture2D
 
 func _ready() -> void:
 	add_to_group("bullets")
 	player = get_tree().get_first_node_in_group("player")
-	if player:
-		direction = global_position.direction_to(player.global_position)
-		rotation = direction.angle()
+	
 	if bullet_mask_type == 1:
-		sprite.modulate = Color(1, 0, 0)
+		sprite.texture = texture_red
 	elif bullet_mask_type == 2:
-		sprite.modulate = Color(0, 0.5, 1, 1)
+		sprite.texture = texture_blue 
 	else:
-		sprite.modulate = Color(1, 1, 1)
+		pass 
 
 func _physics_process(delta: float) -> void:
-	if player:
-		if player.mask_type == bullet_mask_type or player.mask_type == 0:
-			var target_angle = global_position.direction_to(player.global_position).angle()
-			rotation = lerp_angle(rotation, target_angle, 3.0 * delta)
-			direction = Vector2.RIGHT.rotated(rotation)
-		else:
-			rotation = direction.angle()
-	global_position += direction * speed * delta
+	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
